123431f38454e66dbbeffd9d59fd68f8cb2939cb
MediaFace-at <office@mediaface.at>
2022-05-21 19:13:29 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>GAME</h1>
+      <p>CARDS</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-qnxiex
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-qnxiex)
